6e301e524515dc9f57535aaedc586b28498ca0bb
oharar <bohara@senckenberg.de>
2016-03-22 12:01:18 +0100

    Update code to get species data (including names of files with 120-stop data)

--- data-raw/WriteData.R
+++ data-raw/WriteData.R
@@ -11,11 +11,13 @@ Files <-strsplit(FileString, "\n")[[1]]
 Files <-gsub("Species.csv","",Files[grep(".csv", Files)])
   
 # This is unstable, hence splitting it into 4. Should work out how to do error handling properly
-Tables.1 <- sapply(Files[1:5], GetList, dir=dir, sleep=1, simplify=FALSE)
-Tables.2 <- sapply(Files[6:10], GetList, dir=dir, sleep=1, simplify=FALSE)
-Tables.3 <- sapply(Files[11:15], GetList, dir=dir, sleep=1, simplify=FALSE)
-Tables.4 <- sapply(Files[16:19], GetList, dir=dir, sleep=1, simplify=FALSE)
-  
-SpCodes <- rbind(do.call(rbind, Tables.1), do.call(rbind, Tables.2), do.call(rbind, Tables.3), do.call(rbind, Tables.4))
+Tables.1 <- sapply(Files[1:5], GetList, sleep=1, simplify=FALSE)
+Tables.2 <- sapply(Files[6:10], GetList, sleep=1, simplify=FALSE)
+Tables.3 <- sapply(Files[11:15], GetList, sleep=1, simplify=FALSE)
+Tables.4 <- sapply(Files[16:19], GetList, sleep=1, simplify=FALSE)
+SpFiles <- rbind(do.call(rbind, Tables.1), do.call(rbind, Tables.2), do.call(rbind, Tables.3), do.call(rbind, Tables.4))
+
+SpNames <- GetSpNames()
+SpCodes <- merge(SpNames, SpFiles[,c("AOU", "FileString")], all=TRUE, by="AOU")
 
 use_data(SpCodes, overwrite=TRUE)
