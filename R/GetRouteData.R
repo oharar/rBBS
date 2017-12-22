2c73a9a37a4688f2ef27130edfc073b8481c5087
Bob OHara <bob.ohara@ntnu.no>
2017-12-22 14:56:36 +0100

    Add debug to GetRouteData() when there is no data

--- R/GetRouteData.R
+++ R/GetRouteData.R
@@ -17,7 +17,7 @@ GetRouteData=function(AOU=NULL, countrynum=NULL, states=NULL, year, weather=NULL
   if(!is.null(countrynum) & any(!(countrynum%in%c(124, 484, 840)))) stop("countrynum should be either 124 (Canada), 484 (Mexico), or 840 (USA)")
   
   GetDat <- function(file, dir, year, AOU, countrynum, states) {
-    dat=GetUnzip(ZipName=paste0(dir, file), FileName=gsub("^Fifty", "fifty", gsub("zip", "csv", file)))
+    dat <- GetUnzip(ZipName=paste0(dir, file), FileName=gsub("^Fifty", "fifty", gsub("zip", "csv", file)))
     names(dat) <- tolower(names(dat))
     if(is.null(year)) {  UseYear <- TRUE  } else {  UseYear <- dat$year%in%year  }
     if(is.null(AOU)) {  UseAOU <- TRUE  } else {  UseAOU <- dat$aou%in%AOU  }
@@ -26,7 +26,7 @@ GetRouteData=function(AOU=NULL, countrynum=NULL, states=NULL, year, weather=NULL
     Use <- UseYear & UseAOU & UseCountry & UseState
     if(sum(Use)>0) {
       dat$routeID <- paste(dat$statenum, dat[,grep("^[Rr]oute$", names(dat))])
-      dat=subset(dat, subset=Use)
+      dat <-subset(dat, subset=Use)
       return(dat)      
     } else return(NULL)
   }
@@ -55,35 +55,64 @@ GetRouteData=function(AOU=NULL, countrynum=NULL, states=NULL, year, weather=NULL
   if(any(is.na(Files))) warning(paste0("No data for these states: ", paste(RegionsForZipFiles$'State/Prov/TerrName'[Missing], collapse=", ")))
   
   Data.lst <- sapply(Files[!is.na(Files)], GetDat, dir=DirData, year=year, AOU=AOU, countrynum=countrynum, states=states, simplify=FALSE)
-  Data <- ldply(Data.lst)
   
-  # Get route data for all routes, and annual data
-  if(is.null(weather)) weather=GetWeather(Dir)
-  if(is.null(year)) {  UseYear <- TRUE  } else {  UseYear <- weather$Year%in%year  }
-  if(is.null(countrynum)) {  UseCountry <- TRUE  } else {  UseCountry <- weather$countrynum%in%countrynum  }
-  if(is.null(states)) {  UseState <- TRUE  } else {  UseState <- weather$statenum%in%states  }
-  UseWeather <- UseYear & UseCountry & UseState
-  
-  if(is.null(routes)) routes=GetRoutes(Dir)
-  if(is.null(countrynum)) {  UseCountry <- TRUE  } else {  UseCountry <- routes$countrynum%in%countrynum  }
-  if(is.null(states)) {  UseState <- TRUE  } else {  UseState <- routes$statenum%in%states  }
-  UseRoutes <- UseCountry & UseState
-  
-  CommonNames <- names(Data)[names(Data)%in%names(weather)]
-  CommonNames <- CommonNames[CommonNames%in%names(routes)]
-  
-  # Subset data
-  # First, sites sampled in chosen year(s)
-  weather=subset(weather, subset=UseWeather, 
-                 select=c(CommonNames, "Year", "Month", "Day", "RunType"))
-  # Route data for sites sampled in chosen years
-  routes=subset(routes, subset=UseRoutes & routes$routeID%in%weather$routeID, select=c(CommonNames, "Latitude", "Longitude"))
-  
-  AllData=merge(Data, weather, all=TRUE) # by=c("routeID", "RPID"), 
-  AllData=merge(AllData, routes, all=TRUE) # by="routeID", 
-  AllData$SumCount <- apply(AllData[,grep(CountString, names(AllData))],1,sum, na.rm=TRUE)
-  if(!Zeroes) AllData <- subset(AllData, AllData$SumCount>0)
-  AllData <- AllData[,!names(AllData)%in%c(".id", "routedataid", "year")]
+  if(all(unlist(lapply(Data.lst, is.null)))) {
+    warning("no data, sorry")
+    AllData <- NULL
+  } else {
+    Data <- ldply(Data.lst)
+# Get route data for all routes, and annual data
+    if(is.null(weather)) weather <-GetWeather(Dir)
+    if(is.null(year)) {  UseYear <- TRUE  } else {  UseYear <- weather$Year%in%year  }
+    if(is.null(countrynum)) {  UseCountry <- TRUE  } else {  UseCountry <- weather$countrynum%in%countrynum  }
+    if(is.null(states)) {  UseState <- TRUE  } else {  UseState <- weather$statenum%in%states  }
+    UseWeather <- UseYear & UseCountry & UseState
+    
+    if(is.null(routes)) routes <- GetRoutes(Dir)
+    if(is.null(countrynum)) {  UseCountry <- TRUE  } else {  UseCountry <- routes$countrynum%in%countrynum  }
+    if(is.null(states)) {  UseState <- TRUE  } else {  UseState <- routes$statenum%in%states  }
+    UseRoutes <- UseCountry & UseState
+    
+    CommonNames <- names(Data)[names(Data)%in%names(weather)]
+    CommonNames <- CommonNames[CommonNames%in%names(routes)]
+    
+    # Subset data
+    # First, sites sampled in chosen year(s)
+    weather <-subset(weather, subset=UseWeather, 
+                     select=c(CommonNames, "Year", "Month", "Day", "RunType"))
+    # Route data for sites sampled in chosen years
+    routes <- subset(routes, subset=UseRoutes & routes$routeID%in%weather$routeID, 
+                     select=c(CommonNames, "Latitude", "Longitude"))
+    
+  # merge data sets
+    dat.routeID.year <- paste(Data$routeID, Data$year, sep=".")
+    weather.routeID.year <- paste(weather$routeID, weather$Year, sep=".")
+    
+    GetID <- function(datID, otherIDs) {
+      if(length(datID)!=1) stop("datID should be a scalar")
+      wh <- which(datID==otherIDs)
+      if(length(wh)!=1) {
+        if(length(wh)==0) {
+          warning("no ID, so setting to NA")
+        } else {
+          warning("no unique ID, so using first value")
+        }
+        wh <- wh[1]
+      }
+      wh
+    }
+    WeatherWhiches <- sapply(dat.routeID.year, GetID, otherIDs=weather$routeID.year)
+    RouteWhiches <- sapply(Data$routeID, GetID, otherIDs=routes$routeID)
+    
+    AllData <- cbind(Data, weather[WeatherWhiches, !names(weather)%in%names(Data)],
+                     routes[RouteWhiches, !names(routes)%in%names(Data)])
+    
+  #  if(!is.na(weather)) AllData <- merge(Data, weather, all=TRUE) # by=c("routeID", "RPID"), 
+  #  if(!is.na(routes))  AllData <- merge(AllData, routes, all=TRUE) # by="routeID", 
+    AllData$SumCount <- apply(AllData[,grep(CountString, names(AllData))],1,sum, na.rm=TRUE)
+    if(!Zeroes) AllData <- subset(AllData, AllData$SumCount>0)
+    AllData <- AllData[,!names(AllData)%in%c(".id", "routedataid", "year")]
+  }
 
   AllData
 }
